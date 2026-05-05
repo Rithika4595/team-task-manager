1ed98d2534213ad220d62a89ab2907ed0b8266c1
Rithika4595 <rithikadurai771@gmail.com>
2026-05-05 23:00:12 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import api from '../api/axios';
+import api from '../api/Axios';
 import { useAuthStore } from '../store/authStore';
 
 export default function Signup() {
