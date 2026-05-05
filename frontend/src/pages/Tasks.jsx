03dc354e87bf419c6867b19488c0334253a0dee5
Rithika4595 <rithikadurai771@gmail.com>
2026-05-05 23:01:13 +0530

    Update Tasks.jsx

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import api from '../api/axios';
+import api from '../api/Axios';
 import { useAuthStore } from '../store/authStore';
 
 const STATUS_COLORS = {
